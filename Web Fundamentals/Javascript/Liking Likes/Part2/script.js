0f52f302b6ce9029aa674144616ff3a0c12db69f
Sura Qalalwa <suraqalalwa@outlook.com>
2022-09-02 15:54:35 +0300

    first

---
+++ Web Fundamentals/Javascript/Liking Likes/Part2/script.js
@@ -0,0 +1,28 @@
+var like=document.querySelector(".one");
+
+var counter=9;
+function add()
+{
+
+    counter++;
+    like.innerText=counter;
+}
+
+var like2=document.querySelector(".two");
+var counter2=12;
+function add2()
+{
+
+    counter2++;
+    like2.innerText=counter2;
+}
+
+var like3=document.querySelector(".three");
+var counter3=9;
+function add3()
+{
+
+    counter3++;
+    like3
+    .innerText=counter3;
+}
