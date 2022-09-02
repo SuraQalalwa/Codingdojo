0df567f8470bd54bf296835ed1e480f15dbf5733
Sura Qalalwa <suraqalalwa@outlook.com>
2022-09-03 00:39:19 +0300

    first commit

--- Web Fundamentals/Javascript/DojoWeather/script.js
+++ Web Fundamentals/Javascript/DojoWeather/script.js
@@ -1,18 +1,25 @@
+/// Remove cookie
 var element = document.querySelector("#cookie");
 function dojo(){
     element.remove();
 }
 
-var fahernheit = document.querySelectorAll(".convert");
-function temperatureConverter(){
-    fahernheit.innerText=math.floor(fahernheit-32)/1.8;
+//temperatureConverter
+
+function celtofr(temp){
+    return Math.round(1.8 * temp + 32);
+}
+function frtocel(temp) {
+    return Math.round( (temp - 32)/1.8);
+}
+function tempConverter(element) {
+    for(var i=1; i<9; i++) {
+        var tempSpan = document.querySelector("#temp" + i);
+        var tempValue = parseInt(tempSpan.innerText);
+        if(element.value == "°C") {
+            tempSpan.innerText = frtocel(tempValue);
+        } else {
+            tempSpan.innerText = celtofr(tempValue);
+        }
     }
-
-
-
-
-
-
-
-
-
+}
