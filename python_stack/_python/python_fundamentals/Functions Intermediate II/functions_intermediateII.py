08f649da361bd8e76ae582e405acd6efb9aea639
Sura Qalalwa <suraqalalwa@outlook.com>
2022-09-13 14:07:35 +0300

    first

--- python_stack/_python/python_fundamentals/Functions Intermediate II/functions_intermediateII.py
+++ python_stack/_python/python_fundamentals/Functions Intermediate II/functions_intermediateII.py
@@ -1,3 +1,4 @@
+#1.Update Values in Dictionaries and Lists
 x = [ [5,2,3], [10,8,9] ] 
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -24,3 +25,43 @@ print(sports_directory)
 # 4.Change the value 20 in z to 30
 z[0]['y']=30
 print(z)
+
+#//////////////////////////////////////////////////////
+
+# 2. Iterate Through a List of Dictionaries
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+def iterateDictionary():
+    for i in students:
+        print("first_name","-",i['first_name'],", ","last_name","=",i['last_name'])
+iterateDictionary()
+
+
+#/////////////
+
+# 3. Get Values From a List of Dictionaries
+
+def iterateDictionary2(key_name, some_list):
+    for i in some_list:
+        print(i[key_name])
+iterateDictionary2('first_name',students)
+iterateDictionary2('last_name',students)
+
+
+#//////////////////////
+
+# 4. Iterate Through a Dictionary with List Values
+dojo = {
+'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(some_dictionary):
+    for i in some_dictionary:
+        print(len(some_dictionary[i]),i.upper())
+        for j in some_dictionary[i]:
+            print(j)
+printInfo(dojo)
