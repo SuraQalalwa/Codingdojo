003c6cc9d61273043d1bbc95a0c4089f885b4d20
Sura Qalalwa <suraqalalwa@outlook.com>
2022-09-21 11:46:36 +0300

    first

--- python_stack/django/django_fundamentals/counter/counter/wsgi.py
+++ python_stack/django/django_fundamentals/counter/counter/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for counter project.
+WSGI config for Counter project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'counter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Counter.settings')
 
 application = get_wsgi_application()
