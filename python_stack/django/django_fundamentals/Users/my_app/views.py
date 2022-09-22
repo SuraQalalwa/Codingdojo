f8e47bf46fd78de9a134003a7232dae64a233a4d
Sura Qalalwa <suraqalalwa@outlook.com>
2022-09-22 15:02:13 +0300

    first

---
+++ python_stack/django/django_fundamentals/Users/my_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render , redirect
+from .models import Users
+
+# Create your views here.
+def index(request):
+    context={
+        'allusers':Users.objects.all()
+    }
+    return render(request,'index.html',context)
+
+def showdata(request):
+    first_name=request.POST['first_name']
+    last_name= request.POST['last_name']
+    email_address= request.POST['email']
+    age= int(request.POST['age'])
+    Users.objects.create(first_name=first_name,last_name=last_name,email_address=email_address,age=age)
+
+    return redirect  ('/')
