fc09ca5180f7221deee7c4b07bc6aefb9e670fae
Sura Qalalwa <suraqalalwa@outlook.com>
2022-09-30 20:08:10 +0300

    first

---
+++ python_stack/_python/python_fundamentals/For Loops Basic I/for_loop_basicsI.py
@@ -0,0 +1,34 @@
+# 1. Print all integers from 0 to 150.
+for x in range (0, 151,1):
+    print(x)
+
+# # 2.Print all the multiples of 5 from 5 to 1,000
+for y in range (5,1001,5):
+    print(5*y)
+
+# 3.Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+for f in range (1,101,1):
+    if (f%5 == 0):
+        print("Coding")
+    if(f%10 == 0):
+        print("Coding Dojo")    
+
+# 4.Add odd integers from 0 to 500,000, and print the final sum.
+count = 0
+for p in range (0,500001,1):
+    count +=p
+print(count)
+
+# 5.Print positive numbers starting at 2018, counting down by fours.
+for r in range (2018,0,-4):
+    if r>0:
+        print(r)
+
+# 6.Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. 
+# For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+lowNum =2
+highNum = 9
+mult = 3
+for i in range(lowNum, highNum+1, 1):
+    if i % mult == 0: 
+       print(i)
