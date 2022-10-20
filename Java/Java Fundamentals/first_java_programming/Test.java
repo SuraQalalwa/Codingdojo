71005443cffb9cf4a2daa37a191f6974c4ebaeeb
Sura Qalalwa <suraqalalwa@outlook.com>
2022-10-20 14:50:40 +0300

    first java commit

---
+++ Java/Java Fundamentals/first_java_programming/Test.java
@@ -0,0 +1,8 @@
+public class Test{
+    public static void main(String[] args){
+        System.out.println("My name is Sura");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Jenin");
+    }
+
+}
