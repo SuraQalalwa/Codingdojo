00604659a69084c30111468d9244578525abb37b
Sura Qalalwa <suraqalalwa@outlook.com>
2022-10-30 11:59:08 +0200

    First

--- Java/Java OOP/Abstract Art/src/com/AbstractArt/Museum.java
+++ Java/Java OOP/Abstract Art/src/com/AbstractArt/Museum.java
@@ -3,8 +3,14 @@ package com.AbstractArt;
 public class Museum {
     public static void main(String[] args) {
     Painting art =new Painting("Amin","Sura", "Easy", "oil");
+    Painting art1 =new Painting("Hala","Sura", "Easy", "oil");
+    Painting art2 =new Painting("Tareq","Sura", "Easy", "oil");
     Sculpture sculpture = new Sculpture("Dark", "Blue", "Hard", "Marble");
+    Sculpture sculpture1 = new Sculpture("Lox", "Rrd", "Hard", "Marble");
         art.viewArt();
+        art1.viewArt();
+        art2.viewArt();
         sculpture.viewArt();
+        sculpture1.viewArt();
     }
 }
